2de91a4898c49984322f1212d16f9a2e0049348f
felixindrawan <indrawan.felix123@gmail.com>
2024-09-26 07:52:14 -0700

    fix: updated links

--- label-recognizer-js-index/js/index.js
+++ label-recognizer-js-index/js/index.js
@@ -2,8 +2,7 @@ const currentYear = document.querySelector(".current-year");
 window.addEventListener("DOMContentLoaded", () => currentYear.append(new Date().getUTCFullYear()));
 
 const DEMO_LINKS = {
-  "passport-mrz": "https://demo.dynamsoft.com/solutions/passport-scanner/",
-  "general-mrz": "https://demo.dynamsoft.com/Samples/DCV/JS/general-MRZ-scanner/index.html",
+  "general-mrz": "https://demo.dynamsoft.com/solutions/mrz-scanner/index.html",
   vin: "https://demo.dynamsoft.com/Samples/DCV/JS/VINScanner/index.html",
 };
 const router = document.querySelectorAll(".option");
